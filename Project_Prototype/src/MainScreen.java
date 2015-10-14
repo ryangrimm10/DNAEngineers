761fd4c54512d4e45ce957816bd63047bb331f3f
ryangrimm10 <ryan.grimm10@gmail.com>
2015-10-14 15:53:09 -0400

    added colors and stuff

--- Project_Prototype/src/MainScreen.java
+++ Project_Prototype/src/MainScreen.java
@@ -43,10 +43,16 @@ public class MainScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public MainScreen() {
+		Color lightBlue = new Color(0x74AFAD);
+		Color lightOrange = new Color(0xFFAD00);
+		Color okGreen = new Color(0x18F63F);
+		Color radMagenta = new Color(0xA923F3);
+		Color transparent = new Color(0,0,0,0);
+		
 		setTitle("DNA Engineers - Palindrome Sequencer");
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 582, 454);
+		setBounds(100, 100, 500, 500);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -76,9 +82,13 @@ public class MainScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		contentPane.setBackground(Color.RED);
+		
 		JPanel panel_2 = new JPanel();
 		contentPane.add(panel_2, BorderLayout.SOUTH);
 		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_2.setOpaque(false);
+		panel_2.setBackground(transparent);
 		
 		JButton btnNewButton_4 = new JButton("New button");
 		panel_2.add(btnNewButton_4);
@@ -86,33 +96,47 @@ public class MainScreen extends JFrame {
 		JPanel panel_4 = new JPanel();
 		contentPane.add(panel_4, BorderLayout.CENTER);
 		panel_4.setLayout(new GridLayout(6, 2, 0, 0));
+		panel_4.setOpaque(false);
+		panel_4.setBackground(transparent);
+		
 		
 		JPanel panel_5 = new JPanel();
 		panel_4.add(panel_5);
 		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_5.setBackground(transparent);
+		panel_5.setOpaque(false);
 		
-		JLabel lblPalindromicSequencer = new JLabel("Palindromic Sequencer");
-		lblPalindromicSequencer.setForeground(Color.BLACK);
-		lblPalindromicSequencer.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		panel_5.add(lblPalindromicSequencer);
+				
+				JLabel lblPalindromicSequencer = new JLabel("Palindromic Sequencer");
+				lblPalindromicSequencer.setForeground(Color.BLACK);
+				lblPalindromicSequencer.setFont(new Font("Nirmala UI", Font.BOLD, 25));
+				panel_5.add(lblPalindromicSequencer);
 		
 		JPanel panel_6 = new JPanel();
+		panel_6.setOpaque(false);
 		panel_4.add(panel_6);
 		panel_6.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_6.setBackground(lightBlue);
+
 		
 		JLabel lblStrandType = new JLabel("Strand Type:");
 		lblStrandType.setForeground(Color.BLACK);
 		panel_6.add(lblStrandType);
 		
 		JRadioButton rdbtnDna = new JRadioButton("DNA");
+		rdbtnDna.setForeground(new Color(0, 128, 0));
 		rdbtnDna.setSelected(true);
 		panel_6.add(rdbtnDna);
 		
 		JRadioButton rdbtnRna = new JRadioButton("RNA");
+		rdbtnRna.setForeground(radMagenta);
 		panel_6.add(rdbtnRna);
 		
 		JPanel panel_7 = new JPanel();
+		panel_7.setOpaque(false);
 		panel_4.add(panel_7);
+		panel_7.setBackground(lightBlue);
+
 		
 		JLabel lblWhatDo = new JLabel("Find or Compare:");
 		lblWhatDo.setForeground(Color.BLACK);
@@ -127,8 +151,11 @@ public class MainScreen extends JFrame {
 		panel_7.add(rdbtnCompare);
 		
 		JPanel panel_8 = new JPanel();
+		panel_8.setOpaque(false);
 		panel_4.add(panel_8);
 		panel_8.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_8.setBackground(lightBlue);
+
 		
 		JLabel lblNewLabel = new JLabel("First Strand:");
 		panel_8.add(lblNewLabel);
@@ -141,8 +168,11 @@ public class MainScreen extends JFrame {
 		panel_8.add(btnNewButton);
 		
 		JPanel panel_9 = new JPanel();
+		panel_9.setOpaque(false);
 		panel_4.add(panel_9);
 		panel_9.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_9.setBackground(lightBlue);
+
 		
 		JLabel lblNewLabel_1 = new JLabel("Second Strand:");
 		panel_9.add(lblNewLabel_1);
@@ -155,15 +185,17 @@ public class MainScreen extends JFrame {
 		panel_9.add(btnNewButton_1);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setOpaque(false);
 		panel_4.add(panel_1);
 		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_1.setBackground(lightBlue);
 		
 		JButton btnNewButton_2 = new JButton("Submit");
-		btnNewButton_2.setBackground(Color.GREEN);
+		btnNewButton_2.setBackground(okGreen);
 		panel_1.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Clear");
-		btnNewButton_3.setBackground(Color.RED);
+		btnNewButton_3.setBackground(lightOrange);
 		panel_1.add(btnNewButton_3);
 	}
 
